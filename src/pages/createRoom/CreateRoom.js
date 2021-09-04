582b02fa32cbef0e46b99d7c1599c9e75ff202c9
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-09-05 00:53:07 +0300

    Edit - Wrap forms with formWrapper

--- src/pages/createRoom/CreateRoom.js
+++ src/pages/createRoom/CreateRoom.js
@@ -1,44 +1,46 @@
 import * as Yup from 'yup'
-import {React,useEffect,useState} from 'react'
-import { Form,Formik } from 'formik'
-import { Button  } from 'semantic-ui-react'
+import { React, useEffect, useState } from 'react'
+import { Form, Formik } from 'formik'
+import { Button } from 'semantic-ui-react'
 import CustomTextInput from '../../utilities/customForm/CustomTextInput'
 import CustomSelectInput from '../../utilities/customForm/CustomSelectInput'
 import LanguageService from '../../services/languageService'
 import RoomService from '../../services/roomService'
 import { useHistory } from 'react-router'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
+import FormTitle from '../../utilities/customForm/FormTitle'
+import FormWrapper from '../../components/formWrapper/FormWrapper'
 export default function CreateRoom() {
-    const  [languages,setLanguages] = useState({})
+    const [languages, setLanguages] = useState({})
     const history = useHistory()
-    const initialValues = {title:"",limit:0,level:0}
-    let langObject={}
-    
+    const initialValues = { title: "", limit: 0, level: 0 }
+    let langObject = {}
+
 
     const schema = Yup.object({
 
-        title:Yup.string().required().min(4).max(20),
-        limit:Yup.number().required().min(1).max(10),
-        level:Yup.number().required().min(1).max(5),
-        language:Yup.string().required().min(2).max(2)
-    })  
+        title: Yup.string().required().min(4).max(20),
+        limit: Yup.number().required().min(1).max(10),
+        level: Yup.number().required().min(1).max(5),
+        language: Yup.string().required().min(2).max(2)
+    })
 
 
     useEffect(async () => {
         let languageService = new LanguageService()
-        
+
 
         let langs = await languageService.getLanguages()
 
-        
-        langs.data.data.map(lang=>{
+
+        langs.data.data.map(lang => {
 
             langObject[lang.code] = lang.name
-            
+
         })
 
         setLanguages(langObject)
-        
+
 
     }, [])
 
@@ -46,51 +48,55 @@ export default function CreateRoom() {
 
 
 
-    async function createRoom(values){
+    async function createRoom(values) {
 
         //document.getElementById("createRoomSubmit").setAttribute('disabled',true)
         let roomService = new RoomService()
-        let result  =await  roomService.createRoom(values)
-        
+        let result = await roomService.createRoom(values)
+
         console.log(result.data)
 
-        if(result.data.success){
+        if (result.data.success) {
             let roomId = result.data.data._id;
 
-            //history.push(`/rooms/${roomId}`)
-            
+            history.push(`/rooms/${roomId}`)
+
             toast.success("Room is created successfully")
-            
+
         }
 
 
 
-        
+
     }
 
 
 
 
     return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={schema}
-            onSubmit={(values)=>createRoom(values)}
+        <FormWrapper title="Create A Room !">
             
-        >   
-        
-            <Form className="ui form">
-                <CustomTextInput className="mb-5" name="title" placeholder="Title"   ></CustomTextInput>
-                <CustomSelectInput  className="mb-5" name="language" options={languages} default="Langugage"></CustomSelectInput>
-                <CustomSelectInput  className="mb-5" name="level" options={{"1":"Beginner","2":"Elementary","3":"Intermediate","4":"Advanced"}} default="Level"></CustomSelectInput>
-                <CustomSelectInput  className="mb-5" name="limit" options={{"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10}} default="User Limit"></CustomSelectInput>
-                
-                <button type="submit" id="createRoomSubmit" className="btn btn-primary" >Create Room</button>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={schema}
+                onSubmit={(values) => createRoom(values)}
+
+            >
+
+                <Form className="ui form">
+
+                    <CustomTextInput className="mb-5" name="title" placeholder="Title"   ></CustomTextInput>
+                    <CustomSelectInput className="mb-5" name="language" options={languages} default="Langugage"></CustomSelectInput>
+                    <CustomSelectInput className="mb-5" name="level" options={{ "1": "Beginner", "2": "Elementary", "3": "Intermediate", "4": "Advanced" }} default="Level"></CustomSelectInput>
+                    <CustomSelectInput className="mb-5" name="limit" options={{ "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10 }} default="User Limit"></CustomSelectInput>
+
+                    <button type="submit" id="createRoomSubmit" className="btn btn-primary" >Create Room</button>
 
 
-            </Form>
+                </Form>
 
-        </Formik>   
+            </Formik>
+        </FormWrapper>
     )
 
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,57 +1,62 @@
-import {React,useState,useEffect} from 'react'
-import { Form,Formik } from 'formik'
+import { React, useState, useEffect } from 'react'
+import { Form, Formik } from 'formik'
 import * as Yup from 'yup'
 import CustomTextInput from '../../utilities/customForm/CustomTextInput'
 import UserService from '../../services/userService'
 import { Redirect, useHistory } from 'react-router'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
-import {login} from '../../store/action/userAction'
-
+import { login } from '../../store/action/userAction'
+import FormTitle from '../../utilities/customForm/FormTitle'
+import SubmitButton from '../../utilities/customForm/SubmitButton'
+import FormWrapper from '../../components/formWrapper/FormWrapper'
+import { Link } from 'react-router-dom'
+import styles from './Login.module.css'
+import { toast } from 'react-toastify'
 
 export default function Login() {
-    const isUserLoggedIn = useSelector((state)=>state.user)
+    const isUserLoggedIn = useSelector((state) => state.user)
     const dispatch = useDispatch()
     const history = useHistory()
-    const initialValues= {
-        username:"",
-        password:""
+    const initialValues = {
+        username: "",
+        password: ""
     }
 
     const schema = Yup.object({
 
-        username:Yup.string().required().min(3).max(18),
-        password:Yup.string().required()
+        username: Yup.string().required().min(3).max(18),
+        password: Yup.string().required()
 
 
     });
 
-    useEffect(()=>{
-        
-
-       if(isUserLoggedIn){
-           history.push("/")
-       }
-
-    },[isUserLoggedIn])
+    useEffect(() => {
 
 
-    async function handleLogin(values){
+        if (isUserLoggedIn) {
+            history.push("/")
+        }
+
+    }, [isUserLoggedIn])
+
+
+    async function handleLogin(values) {
 
         let userService = new UserService()
-        let loginResult =await  userService.login(values.username,values.password)
-        loginResult=loginResult.data
+        let loginResult = await userService.login(values.username, values.password)
+        loginResult = loginResult.data
 
-        if(loginResult.success){
+        if (loginResult.success) {
 
             let accessToken = loginResult.data.accessToken;
-            let refreshToken= loginResult.data.refreshToken;
+            let refreshToken = loginResult.data.refreshToken;
 
-             dispatch(login({accessToken,refreshToken}))
-            
-        }else{
+            dispatch(login({ accessToken, refreshToken }))
 
-            alert("username or password is wrong")
+        } else {
+
+            toast.error("username or password is wrong",{autoClose:10000})
 
         }
 
@@ -64,30 +69,40 @@ export default function Login() {
         <>
 
 
-        {isUserLoggedIn?
+            {isUserLoggedIn ?
 
 
-        <Redirect to="/"></Redirect>:
+                <Redirect to="/"></Redirect> :
+                <FormWrapper title="LOGIN">
+
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={schema}
+                        onSubmit={(values) => handleLogin(values)}
+
+                    >
+
+                        <Form className="form-row ui form">
+                            <CustomTextInput className="mb-5 " name="username" placeholder="E-mail or username"   ></CustomTextInput>
+                            <CustomTextInput className="mb-5 " name="password" placeholder="Password" type="password"   ></CustomTextInput>
+                            <div class="row justify-content-center">
+                                <SubmitButton text="Login" className="col-12 col-md-8 col-lg-8 btn btn-primary" ></SubmitButton>
+                                <div className="row justify-content-center">
+
+                                    <Link to="/register" className={`col-12 text-center mt-3 ${styles.link}`}>Create an account</Link>
+                                </div>
+                            </div>
+                        </Form>
+
+                    </Formik>
 
 
-            <Formik
-                initialValues={initialValues}
-                validationSchema={schema}
-                onSubmit={(values)=>handleLogin(values)}
-
-            >   
-            
-                <Form className="ui form">
-                    <CustomTextInput className="mb-5" name="username" placeholder="E-mail or username"   ></CustomTextInput>
-                    <CustomTextInput className="mb-5" name="password" placeholder="Password" type="password"   ></CustomTextInput>
-                        
-                    <button type="submit" className="btn btn-primary">Create Room</button>
-                </Form>
-
-            </Formik>   
+                </FormWrapper>
 
 
-        }
+
+
+            }
         </>
 
 
