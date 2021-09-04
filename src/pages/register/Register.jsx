599f1bf217f8f4fb89128c1e988d194a9eea55a0
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-09-05 00:52:19 +0300

    Edit register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,12 +2,16 @@ import { React, useState, useEffect } from 'react'
 import { Form, Formik } from 'formik'
 import * as Yup from 'yup'
 import CustomTextInput from '../../utilities/customForm/CustomTextInput'
+import FormTitle from '../../utilities/customForm/FormTitle'
 import UserService from '../../services/userService'
 import { Redirect, useHistory } from 'react-router'
 import { useSelector, useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
 import { login } from '../../store/action/userAction'
-
+import SubmitButton from '../../utilities/customForm/SubmitButton'
+import { Link } from 'react-router-dom'
+import styles from './Register.module.css'
+import FormWrapper from '../../components/formWrapper/FormWrapper'
 
 export default function Login() {
     const isUserLoggedIn = useSelector((state) => state.user)
@@ -63,42 +67,34 @@ export default function Login() {
 
 
     return (
-        <div class="row">
 
 
-
-            <div class="col-12 ">
-                <div class="row d-flex justify-content-center">
+        <FormWrapper title="REGISTER">
 
 
-                    <div class="col-12 text-center p-3">
-                        <h2>Register Speakiv</h2>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={schema}
+                onSubmit={(values) => handleRegister(values)}
+            >
+
+                <Form className="form-row  ui form ">
+                    <CustomTextInput className="col-12 mb-5" name="name" placeholder="Name"   ></CustomTextInput>
+                    <CustomTextInput className="col-12 mb-5" name="username" placeholder="Username"   ></CustomTextInput>
+                    <CustomTextInput className="col-12 mb-5" name="email" placeholder="Email"></CustomTextInput>
+                    <CustomTextInput className="col-12 mb-5" name="password" placeholder="Password" type="password"   ></CustomTextInput>
+                    <div class="row justify-content-center">
+                        <SubmitButton text="Register" className="col-12 col-md-8 col-lg-8 btn btn-primary"></SubmitButton>
+                        <div className="row justify-content-center">
+
+                            <Link to="/login" className={`col-12 text-center mt-3 ${styles.link}`}>Do you have an account?</Link>
+                        </div>
                     </div>
 
-                    <div class="col-6">
+                </Form>
 
-                        <Formik
-                            initialValues={initialValues}
-                            validationSchema={schema}
-                            onSubmit={(values) => handleRegister(values)}
-                        >
+            </Formik>
+        </FormWrapper>
 
-                            <Form className="ui form">
-                                <CustomTextInput className="mb-5" name="name" placeholder="Name"   ></CustomTextInput>
-                                <CustomTextInput className="mb-5" name="username" placeholder="Username"   ></CustomTextInput>
-                                <CustomTextInput className="mb-5" name="email" placeholder="Email"></CustomTextInput>
-                                <CustomTextInput className="mb-5" name="password" placeholder="Password" type="password"   ></CustomTextInput>
-                                <button type="submit" className="btn btn-primary">Create Room</button>
-                            </Form>
-
-                        </Formik>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
     )
 }
