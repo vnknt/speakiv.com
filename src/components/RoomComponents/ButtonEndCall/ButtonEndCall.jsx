8fe447c4e1a86022d1eff65f83066d776022a2f6
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-09-18 02:19:03 +0300

    Add buttons for ending call, end toggling microphone

---
+++ src/components/RoomComponents/ButtonEndCall/ButtonEndCall.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonEndCall(props) {
+    return (
+        <>
+            <button className="text-3xl w-12 h-12 rounded-full bg-red-700 text-white text-center"><i className="phone icon"></i></button>
+        </>
+    )
+}
---
+++ src/components/RoomComponents/ButtonMicrophone/ButtonMicrophone.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonMicrophone(props) {
+    return (
+        <>
+            <button className="text-3xl w-12 h-12 rounded-full bg-green-800 text-gray-300 text-center"><i className="microphone icon"></i></button>
+        </>
+    )
+}
